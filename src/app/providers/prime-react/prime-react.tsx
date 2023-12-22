82ac9d48019d4f6b7d94b86182b45e9af0d04654
nolood <a9514548799@gmail.com>
2023-12-22 18:58:55 +0500

    feat: prime react + css loader

--- src/app/providers/prime-react/prime-react.tsx
+++ src/app/providers/prime-react/prime-react.tsx
@@ -2,7 +2,7 @@ import { type FC, type ReactNode } from "react";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-dark-purple/theme.css";
 
-const PrimeReact: FC = ({ children }: { children: ReactNode }) => {
+const PrimeReact: FC<{ children: ReactNode }> = ({ children }) => {
   return <PrimeReactProvider>{children}</PrimeReactProvider>;
 };
 
